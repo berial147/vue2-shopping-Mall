0f6ab7c6b30a7b5eaee40eea0b52e7e7ee3f06fa
cx1801126003 <2716506732@qq.com>
2022-03-17 16:09:21 +0800

    center page add the second router

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,9 @@ import ShopCart from '../pages/ShopCart'
 import Trade from '../pages/Trade'
 import Pay from '../pages/Pay'
 import PaySuccess from '../pages/PaySuccess'
+import Center from '../pages/Center'
+import MyOrder from '../pages/Center/myOrder'
+import GroupOrder from '../pages/Center/groupOrder'
 
 //路由配置信息
 export default [
@@ -66,6 +69,25 @@ export default [
             path: "/paysuccess",
             component: PaySuccess,
             meta: {isShow: true}
+        },
+        {
+            path: '/center',
+            component: Center,
+            meta: {isShow: true},
+            children: [
+                {
+                    path: 'myorder',
+                    component: MyOrder
+                },
+                {
+                    path: 'grouporder',
+                    component: GroupOrder
+                },
+                {
+                    path: '/center',
+                    redirect: '/center/myorder'
+                }
+            ]
         },
         //重定向，在项目跑起来的时候立马让它跑到首页的位置
 
